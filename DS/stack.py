9b49273b7c07d4a34e3b8b2fbf133b5783157bf0
Amit Kumar Dubey <35802447+amitdu6ey@users.noreply.github.com>
2018-06-01 01:12:28 +0530

    Create stack.py

---
+++ DS/stack.py
@@ -0,0 +1,21 @@
+# Implementing a Stack in Python
+# Problem 1
+
+class Stack:
+	def __init__(self):
+		self.items=[]
+	
+	def push(self, item):
+		self.items.append(item)
+		
+	def pop(self):
+		return self.items.pop()
+	
+	def isEmpty(self):
+		return self.items == []
+	
+	def peek(self):
+		return self.items[len(self.items)-1]
+	
+	def size(self):
+		return len(self.items)
